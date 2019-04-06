6eb588f21f2613a7504acc034e18479ea3655b9b
nidhinjohn1 <nidhinjohn1@gmail.com>
2019-04-06 16:43:07 +0530

    commit user tasks

---
+++ src/app/pages/user-task/service/task.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  //apiURL: string = 'http://www.server.com/api/';
+
+  constructor(private httpClient: HttpClient) {}
+
+  public getProcess(roles:string[]){
+    return this.httpClient.get('http://localhost:8888/onboard/form-fields/?roles='+roles);
+}
+}
--- src/app/pages/user-task/user-task.component.ts
+++ src/app/pages/user-task/user-task.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
+import { NbTokenService , NbAuthJWTToken} from '@nebular/auth';
+import {Injectable} from '@angular/core';
+import { User } from '../../Models/User';
+import {TaskService} from './service/task.service'
 
 @Component({
   selector: 'ngx-user-task',
@@ -23,10 +27,21 @@ export class UserTaskComponent implements OnInit {
   };
 
   source: LocalDataSource = new LocalDataSource();
+  data = [];
 
-  constructor() { 
-    const data = [];
-    this.source.load(data);
+  constructor(private tokenService: NbTokenService,private taskService:TaskService) {
+    this.tokenService.get().subscribe((token: NbAuthJWTToken) => {
+      if (token.isValid()) {
+           let user : User = token.getPayload();
+           console.log(user.roles)
+           let roles = user.roles
+           this.taskService.getProcess(roles).subscribe((res:any[])=>{
+            this.data = res;
+            this.source.load(this.data);
+           });
+      }
+   });
+    
 
   }
 
